be40a9188ffcbcbf975321084187c4fbc3309420
Laurynas Ruškys <bilbas@gmail.com>
2021-12-28 22:25:15 +0200

    Fix IP

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -input_format h264 -i /dev/video0 -c:v copy -c:a none -f rtp rtp://192.168.1.198:1234
+ffmpeg -f v4l2 -input_format h264 -i /dev/video0 -c:v copy -c:a none -f rtp rtp://192.168.1.233:1234
