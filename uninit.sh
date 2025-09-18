d1f154f463049d0105e6a7f00b3c60b4f97d749d
Laurynas Ruškys <bilbas@gmail.com>
2025-09-18 22:12:10 +0300

    Fix uninit

--- uninit.sh
+++ uninit.sh
@@ -7,11 +7,19 @@ FRONT_RIGHT_FORWARD=23
 REAR_RIGHT_BACKWARD=24
 REAR_RIGHT_FORWARD=25
 
-echo $FRONT_LEFT_BACKWARD > /sys/class/gpio/unexport
-echo $FRONT_LEFT_FORWARD > /sys/class/gpio/unexport
-echo $FRONT_RIGHT_BACKWARD > /sys/class/gpio/unexport
-echo $REAR_LEFT_FORWARD > /sys/class/gpio/unexport
-echo $REAR_LEFT_BACKWARD > /sys/class/gpio/unexport
-echo $FRONT_RIGHT_FORWARD > /sys/class/gpio/unexport
-echo $REAR_RIGHT_BACKWARD > /sys/class/gpio/unexport
-echo $REAR_RIGHT_FORWARD > /sys/class/gpio/unexport
+# echo $FRONT_LEFT_BACKWARD > /sys/class/gpio/unexport
+# echo $FRONT_LEFT_FORWARD > /sys/class/gpio/unexport
+# echo $FRONT_RIGHT_BACKWARD > /sys/class/gpio/unexport
+# echo $REAR_LEFT_FORWARD > /sys/class/gpio/unexport
+# echo $REAR_LEFT_BACKWARD > /sys/class/gpio/unexport
+# echo $FRONT_RIGHT_FORWARD > /sys/class/gpio/unexport
+# echo $REAR_RIGHT_BACKWARD > /sys/class/gpio/unexport
+# echo $REAR_RIGHT_FORWARD > /sys/class/gpio/unexport
+pinctrl set $FRONT_LEFT_FORWARD op pd dl
+pinctrl set $FRONT_LEFT_BACKWARD op pd dl
+pinctrl set $FRONT_RIGHT_FORWARD op pd dl
+pinctrl set $FRONT_RIGHT_BACKWARD op pd dl
+pinctrl set $REAR_RIGHT_FORWARD op pd dl
+pinctrl set $REAR_RIGHT_BACKWARD op pd dl
+pinctrl set $REAR_LEFT_FORWARD op pd dl
+pinctrl set $REAR_LEFT_BACKWARD op pd dl
